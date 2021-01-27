3c50def99cba2865e28dcb11d86ad72086b65750
Xujiayao147 <caiandxu@163.com>
2021-01-27 14:04:34 +0800

    修复了打包后出现HTTPS报错的问题

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module GIFSignaturesGenerator {
 	requires net.sf.image4j;
 	requires org.dtools.javaini;
 	requires commons.io;
+	requires jdk.crypto.ec;
 
 	exports io.gitee.xujiayao147.gifSignaturesGenerator;
 	exports io.gitee.xujiayao147.gifSignaturesGenerator.ui;
