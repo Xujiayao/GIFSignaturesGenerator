be4ae23f013f9ea088da00741014abc177c22af6
Xujiayao147 <caiandxu@163.com>
2021-05-12 15:34:27 +0800

    将GitPusher.bat纳入追踪的文件

---
+++ GitPusher.bat
@@ -0,0 +1,10 @@
+@echo off
+title Git Pusher By Xujiayao
+cd "C:\Users\Xujiayao\Desktop\Java Projects\GIFSignaturesGenerator"
+git add .
+echo Enter commit message: 
+set /p commit_message=
+git commit -m "%commit_message%"
+git push origin master
+start https://purge.jsdelivr.net/gh/Xujiayao147/GIFSignaturesGenerator@master/update/version.json
+pause
